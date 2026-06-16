53d08694579e755f15a8acccd27ab3a0f8fbeb81
Shayan <shayan.circ@gmail.com>
2026-06-16 16:49:22 +0530

    Replicate applied

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI, UploadFile, File, Form
 from typing import Optional
+from dotenv import load_dotenv
+import replicate
+
+# Load .env file
+load_dotenv()
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
     return {
         "message": "SketchAI Backend Running"
     }
+
 
 @app.post("/generate-image")
 async def generate_image(
@@ -22,4 +29,19 @@ async def generate_image(
             if reference_image
             else None
         )
+    }
+
+
+@app.get("/test-image")
+def test_image():
+
+    output = replicate.run(
+        "openai/gpt-image-2",
+        input={
+            "prompt": "A futuristic cyberpunk city at sunset"
+        }
+    )
+
+    return {
+        "output": output
     }
